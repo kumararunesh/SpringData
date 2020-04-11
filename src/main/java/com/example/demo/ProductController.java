07a77d780aee746a06208b881d6bc50353275af5
Kumar Arunesh <kumararunesh211994@gmail.com>
2020-04-11 15:49:14 +0530

    d

--- src/main/java/com/example/demo/ProductController.java
+++ src/main/java/com/example/demo/ProductController.java
@@ -1,10 +1,7 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -13,6 +10,12 @@ public class ProductController {
 
     @Autowired
     private ProductService productService;
+
+    @RequestMapping("/hi")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
 
     @GetMapping("/products")
     public List<Product> getAllProducts()
